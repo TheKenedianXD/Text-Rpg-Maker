7f9cd2976c1f00c21e4a775913ab555ea6aa2c1d
JohnnyBeanzz <TheKenedianXD@gmail.com>
2025-03-01 23:12:28 +0100

    feat: modified the stat service to allow negative multipliers (dividing), removed Increasing as we can just simply use + -, added descriptions to show what the primary stat affects in the game later

--- TextRpg.Core/Models/Data/Character/StatModel.cs
+++ TextRpg.Core/Models/Data/Character/StatModel.cs
@@ -9,16 +9,15 @@ namespace TextRpg.Core.Models.Data.Character
         [Description("The name of the stat (e.g., Strength, Dexterity, Stamina).")]
         public string Name { get; set; } = "";
 
-        [Description("Indicates whether increasing this stat is beneficial.")]
-        public bool Increases { get; set; }
+        [Description("A short description of what this stat does and what it affects.")]
+        public string Description { get; set; } = "";
 
         [Description("A dictionary that maps affected base stats to their effect value and type.")]
         public Dictionary<BaseStat, (float Value, bool IsMultiplier)> Affects { get; set; } = [];
 
-        public StatModel(string name, bool increases, Dictionary<BaseStat, (float, bool)> affects)
+        public StatModel(string name, Dictionary<BaseStat, (float, bool)> affects)
         {
             Name = name;
-            Increases = increases;
             Affects = affects;
         }
     }
